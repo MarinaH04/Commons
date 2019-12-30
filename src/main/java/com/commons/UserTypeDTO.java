1053d400866ec35e34b11c68c67cae01dde15485
Marina Hanzu <hanzumarina@yahoo.com>
2019-12-30 12:16:47 +0200

    usertype

--- src/main/java/com/commons/UserTypeDTO.java
+++ src/main/java/com/commons/UserTypeDTO.java
@@ -1,7 +1,16 @@
 package com.commons;
 
 public class UserTypeDTO {
+	private String username;
 	private String tip;
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
 	public String getTip() {
 		return tip;
@@ -13,6 +22,8 @@ public class UserTypeDTO {
 
 	@Override
 	public String toString() {
-		return "UserTypeDTO [tip=" + tip + "]";
+		return "UserTypeDTO [username=" + username + ", tip=" + tip + "]";
 	}
+
+	
 }
